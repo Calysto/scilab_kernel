1271f2aa4042c42d3536960c5bf9ff7d227b8c21
Steven Silvester <steven.silvester@ieee.org>
2014-08-13 17:20:59 -0500

    Install kernelspec at install time using sys.executable.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 from distutils.command.install import install
+import json
 import os
 import sys
 
@@ -9,11 +10,27 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 
+kernel_json = {"argv": [sys.executable, "-m", "scilab_kernel", "-f",
+                        "{connection_file}"],
+               "display_name": "Scilab",
+               "language": "scilab",
+               "codemirror_mode": "Octave"
+               }
+
+
 class install_with_kernelspec(install):
     def run(self):
+        # Regular installation
         install.run(self)
-        from IPython.kernel.kernelspec import install_kernel_spec
-        install_kernel_spec('kernelspec', 'scilab', replace=True)
+
+        # Now write the kernelspec
+        from IPython.kernel.kernelspec import KernelSpecManager
+        from IPython.utils.path import ensure_dir_exists
+        destdir = os.path.join(KernelSpecManager().user_kernel_dir, 'scilab')
+        ensure_dir_exists(destdir)
+        with open(os.path.join(destdir, 'kernel.json'), 'w') as f:
+            json.dump(kernel_json, f, sort_keys=True)
+
 
 with open('README.rst') as f:
     readme = f.read()
